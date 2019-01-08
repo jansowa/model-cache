dda25129d337a5bc593dba8b26f14d85f37fce3c
Jan Sowa <jan.piotr.sowa@gmail.com>
2019-01-08 19:09:22 +0100

    Add all fields and Lombok annotations to FileBasicInfo class

--- src/main/java/com/github/jansowa/domain/FileBasicInfo.java
+++ src/main/java/com/github/jansowa/domain/FileBasicInfo.java
@@ -1,4 +1,17 @@
 package com.github.jansowa.domain;
 
+import lombok.Builder;
+import lombok.Value;
+
+import java.util.Date;
+
+@Value
+@Builder
 public class FileBasicInfo {
+    String name;
+    String filePath;
+    String extension;
+    String url;
+    Date creationTime;
+    Date lastModifiedTime;
 }
