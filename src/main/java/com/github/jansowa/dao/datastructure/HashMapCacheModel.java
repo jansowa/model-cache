be32291fa5eae8c0195480c7b5bb6b0563145343
Jan Sowa <jan.piotr.sowa@gmail.com>
2019-01-29 08:49:54 +0100

    Add HashMap cache implementation with tests

--- src/main/java/com/github/jansowa/dao/datastructure/HashMapCacheModel.java
+++ src/main/java/com/github/jansowa/dao/datastructure/HashMapCacheModel.java
@@ -3,17 +3,18 @@ package com.github.jansowa.dao.datastructure;
 import com.github.jansowa.dao.CacheModel;
 import com.github.jansowa.domain.FileBasicInfo;
 
-import java.io.Serializable;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import java.io.*;
+import java.util.*;
+import java.util.logging.Logger;
 
+
+//TODO(IMPORTANT!!!) serialize data like in ArrayListCacheModel! Make save and load protected functions!
 public class HashMapCacheModel implements CacheModel, Serializable{
     private transient double maxStorageMB;
     private transient String cacheModelPath;
-    private Map<String, FileBasicInfo> storedFiles;
+    private HashMap<String, FileBasicInfo> storedFiles;
 
-    public MapCacheModel(double maxStorageMB, String cacheModelPath){
+    public HashMapCacheModel(double maxStorageMB, String cacheModelPath){
         this.maxStorageMB = maxStorageMB;
         this.cacheModelPath = cacheModelPath;
         this.storedFiles = new HashMap<>();
@@ -21,36 +22,120 @@ public class HashMapCacheModel implements CacheModel, Serializable{
 
     @Override
     public void put(FileBasicInfo file) {
-
+        storedFiles.put(file.getFilePath(), file);
+        while(getSizeInBytes()>1048576*maxStorageMB)
+            remove(findEldestFile());
+        saveData();
     }
 
     @Override
     public void remove(String filePath) {
-
+        storedFiles.remove(filePath);
+        saveData();
     }
 
     @Override
     public boolean contains(String filePath) {
-        return false;
+        return storedFiles.containsKey(filePath);
     }
 
     @Override
     public void movePath(String sourcePath, String destinationPath) {
+        if(storedFiles.containsKey(sourcePath)) {
+            moveSingleFile(sourcePath, destinationPath);
+        }
+        else{
+            moveWholeFolder(sourcePath, destinationPath);
+        }
+        saveData();
+    }
+
+    private void moveSingleFile(String sourcePath, String destinationPath) {
+        FileBasicInfo fileToMove = storedFiles.remove(sourcePath);
+        fileToMove = fileToMove.withLastUsageTime(new Date()).withFilePath(destinationPath);
+        storedFiles.put(destinationPath, fileToMove);
+    }
+
+    private void moveWholeFolder(String sourcePath, String destinationPath) {
+        int sourcePathLength = sourcePath.length();
+        ArrayList<String> pathsToMove = new ArrayList<>();
+        for (String key:storedFiles.keySet()) {
+            if(key.substring(0, sourcePathLength).equals(sourcePath)){
+                pathsToMove.add(key);
+            }
+        }
+        for (String path: pathsToMove) {
+            moveSingleFile(path, destinationPath+path.substring(sourcePathLength));
+        }
 
     }
 
     @Override
     public Optional<FileBasicInfo> read(String filePath) {
-        return Optional.empty();
+        if (!storedFiles.containsKey(filePath))
+            return Optional.ofNullable(null);
+        FileBasicInfo downloadedFile = storedFiles.get(filePath);
+        downloadedFile.withLastUsageTime(new Date());
+        storedFiles.remove(filePath);
+        storedFiles.put(filePath, downloadedFile);
+        return Optional.ofNullable(storedFiles.get(filePath)); //TODO what if filePath empty?
     }
 
     @Override
     public int getNumberOfFiles() {
-        return 0;
+        return storedFiles.size();
     }
 
     @Override
     public void removeAllData() {
+        storedFiles.clear();
+    }
 
+    @Override
+    public double getSizeInBytes(){
+        File cacheModel = new File(cacheModelPath);
+        return cacheModel.length();
+    }
+
+    public void removeFromDevice(){
+        File cacheModel = new File(cacheModelPath);
+        if(!cacheModel.delete()){
+            System.out.println("File "+cacheModelPath+" doesn't exist!");
+        }
+    }
+
+    protected void saveData(){
+        try (ObjectOutputStream objectOutputStream =
+                     new ObjectOutputStream(
+                             new FileOutputStream(cacheModelPath))){
+            objectOutputStream.writeObject(storedFiles);
+        } catch (IOException e) {
+            Logger logger = Logger.getLogger(ArrayListCacheModel.class.getName());
+            logger.warning("HashMap cache can't save data into device.");
+        }
+    }
+
+    protected void loadData(){
+        try(ObjectInputStream objectInputStream =
+                    new ObjectInputStream(
+                            new FileInputStream(cacheModelPath))){
+
+            this.storedFiles = (HashMap<String, FileBasicInfo>) objectInputStream.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            Logger logger = Logger.getLogger(ArrayListCacheModel.class.getName());
+            logger.warning("HashMap cache can't load data from device.");
+        }
+    }
+
+    private String findEldestFile(){
+        Date eldestDate = new Date(2200, 1, 1);
+        String pathOfEldestFile = null;
+        for (FileBasicInfo file: storedFiles.values()) {
+            if(file.getLastUsageTime().getTime()<eldestDate.getTime()){
+                eldestDate = file.getLastUsageTime();
+                pathOfEldestFile = file.getFilePath();
+            }
+        }
+        return pathOfEldestFile;
     }
 }
---
+++ src/test/groovy/com/github/jansowa/dao/datastructure/HashMapCacheModelSpec.groovy
@@ -0,0 +1,234 @@
+package com.github.jansowa.dao.datastructure
+
+import com.github.jansowa.domain.FileBasicInfo
+import spock.lang.Shared
+import spock.lang.Specification
+
+class HashMapCacheModelSpec extends Specification{
+    @Shared HashMapCacheModel cacheModel
+    @Shared private FileBasicInfo[] sampleData
+    static final double MAX_STORAGE = 2
+    static final String CACHE_PATH = "HashMapCacheModelTest.ser"
+
+    def setupSpec()
+    {
+        sampleData = new FileBasicInfo[3]
+        sampleData[0] = generateFileBasicInfo("sample0", "/test/")
+        sampleData[1] = generateFileBasicInfo("sample1", "/test/")
+        sampleData[2] = generateFileBasicInfo("sample2", "/test/")
+    }
+
+    def setup()
+    {
+        cacheModel = new HashMapCacheModel(MAX_STORAGE, CACHE_PATH)
+    }
+
+    def cleanup()
+    {
+        cacheModel.removeFromDevice()
+    }
+
+    private static FileBasicInfo generateFileBasicInfo(String fileName, String folderPath){
+        Date creationDate = new Date()
+        return new FileBasicInfo(fileName, folderPath+fileName+".txt", "txt", "http://example.com/"+fileName+".txt", creationDate, creationDate)
+    }
+
+    void "Should upload file"()
+    {
+        when:
+        cacheModel.put(sampleData[0])
+
+        then:
+        cacheModel.contains(sampleData[0].getFilePath())
+    }
+
+    void "Should delete uploaded file"()
+    {
+        given:
+        cacheModel.put(sampleData[0])
+
+        when:
+        cacheModel.remove(sampleData[0].getFilePath())
+
+        then:
+        !cacheModel.contains(sampleData[0].getFilePath())
+    }
+
+    void "Should read file"()
+    {
+        given:
+        cacheModel.put(sampleData[0])
+        when:
+        Optional<FileBasicInfo> downloadedInfo = cacheModel.read(sampleData[0].getFilePath())
+        then:
+        downloadedInfo.get().creationTime == sampleData[0].creationTime
+        downloadedInfo.get().name == sampleData[0].name
+    }
+
+    void "Read method should return Optional with null if there is no file in given path"()
+    {
+        given:
+        String readTestPath = "/test/read"
+
+        when:
+        Optional<FileBasicInfo> downloadedInfo= cacheModel.read(readTestPath)
+
+        then:
+        Optional.ofNullable(null) == downloadedInfo
+    }
+
+    void "Contain method should return true if model contains file"()
+    {
+        given:
+        cacheModel.put(sampleData[0])
+
+        when:
+        boolean result = cacheModel.contains(sampleData[0].getFilePath())
+
+        then:
+        result
+    }
+
+    void "Contain method should return false if model doesn't contain file"()
+    {
+        given:
+        String containsTestPath = "/test/contains"
+
+        when:
+        boolean result = cacheModel.contains(containsTestPath)
+
+        then:
+        !result
+    }
+
+    void "Should move single file from first to second path"()
+    {
+        given:
+        String destinationPath = "/test/destination"
+        cacheModel.put(sampleData[0])
+
+        when:
+        cacheModel.movePath(sampleData[0].getFilePath(), destinationPath)
+
+        then:
+        cacheModel.contains(destinationPath)
+        !cacheModel.contains(sampleData[0].filePath)
+    }
+
+    void "Should move whole folder from first to second path"()
+    {
+        given:
+        String[] paths = new String[5]
+        String sourceFolder = "/test/"
+        String destinationFolder = "/testDestination/"
+        for(int i=0; i<3; i++){
+            paths[i] = "move1/"
+        }
+        paths[3] = "move1/move1-1/"
+        paths[4] = "move2/"
+        FileBasicInfo generatedFile
+
+        for(int i=0; i<5; i++){
+            generatedFile = generateFileBasicInfo("file"+i, sourceFolder +paths[i])
+            cacheModel.put(generatedFile)
+        }
+
+        when:
+        cacheModel.movePath(sourceFolder, destinationFolder)
+
+        then:
+        cacheModel.contains(destinationFolder+paths[0]+"file0.txt")
+        cacheModel.contains(destinationFolder+paths[1]+"file1.txt")
+        cacheModel.contains(destinationFolder+paths[2]+"file2.txt")
+        cacheModel.contains(destinationFolder+paths[3]+"file3.txt")
+        cacheModel.contains(destinationFolder+paths[4]+"file4.txt")
+    }
+
+    void "Should get number of stored files"()
+    {
+        given:
+        cacheModel.put(sampleData[0])
+        cacheModel.put(sampleData[1])
+        cacheModel.put(sampleData[2])
+
+        when:
+        int numberOfFiles = cacheModel.getNumberOfFiles()
+
+        then:
+        numberOfFiles == 3
+    }
+
+    void "Should remove all data"()
+    {
+        given:
+        cacheModel.put(sampleData[0])
+        cacheModel.put(sampleData[1])
+
+        when:
+        cacheModel.removeAllData()
+
+        then:
+        !cacheModel.contains(sampleData[0].getFilePath())
+        !cacheModel.contains(sampleData[1].getFilePath())
+        cacheModel.getNumberOfFiles()==0
+    }
+
+    void "Should save cache on device"()
+    {
+        given:
+        File cacheModelFile = new File(CACHE_PATH)
+        cacheModel.put(sampleData[0])
+        when:
+        cacheModel.saveData()
+        then:
+        cacheModelFile.exists()
+    }
+
+    void "Should load cache from device"()
+    {
+        given:
+        cacheModel.put(sampleData[0])
+        cacheModel.put(sampleData[1])
+        cacheModel.saveData()
+        HashMapCacheModel loadedCacheModel = new HashMapCacheModel(MAX_STORAGE, CACHE_PATH)
+        when:
+        loadedCacheModel.loadData()
+        then:
+        loadedCacheModel != null
+        loadedCacheModel
+                .read(sampleData[0]
+                .filePath)
+                .get()
+                .getName() ==
+                sampleData[0].getName()
+        loadedCacheModel
+                .read(sampleData[1]
+                .filePath)
+                .get()
+                .getCreationTime() ==
+                sampleData[1].getCreationTime()
+
+    }
+
+    void "Should remove cache from device"()
+    {
+        given:
+        cacheModel.saveData()
+        when:
+        cacheModel.removeFromDevice()
+        File cacheModelFile = new File(CACHE_PATH)
+        then:
+        !cacheModelFile.exists()
+    }
+
+    void "Should get size of cache model in bytes"()
+    {
+        given:
+        cacheModel.put(sampleData[0])
+        when:
+        double size = cacheModel.getSizeInBytes()
+        then:
+        size>0
+    }
+
+}
