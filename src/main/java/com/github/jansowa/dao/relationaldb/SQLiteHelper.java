05611b1a374d90aefeb8bda9a3828ad609720cc0
Jan Sowa <jan.piotr.sowa@gmail.com>
2019-02-24 13:46:01 +0100

    Add SQLiteHelper class -functions for closing resultsets, connections, statements, preparedstatements

---
+++ src/main/java/com/github/jansowa/dao/relationaldb/SQLiteHelper.java
@@ -0,0 +1,49 @@
+package com.github.jansowa.dao.relationaldb;
+
+import java.sql.*;
+import java.util.logging.Logger;
+
+public final class SQLiteHelper {
+    public static void close(Connection connection){
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            log(e);
+        }
+    }
+
+    public static void close(Statement statement){
+        try{
+            if(statement != null) {
+                statement.close();
+            }
+        } catch (SQLException e) {
+            log(e);
+        }
+    }
+
+    public static void close(PreparedStatement preparedStatement){
+        try{
+            if(preparedStatement != null){
+                preparedStatement.close();
+            }
+        } catch (SQLException e) {
+            log(e);
+        }
+    }
+
+    public static void close(ResultSet resultSet){
+        if(resultSet != null){
+            try {
+                resultSet.close();
+            } catch (SQLException e) {
+                log(e);
+            }
+        }
+    }
+
+    private static void log(Exception e){
+        Logger logger = Logger.getLogger("SQLite close logger");
+        logger.warning(e.toString());
+    }
+}
